254c7a20dd424473db32aec23d5e636e7a193d41
wa1ead <waliddraa2@gmail.com>
2024-09-24 19:14:50 +0100

    edit JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+var box = document.getElementById("colored-box");
+console.log(box);
+var button = document.getElementById("color-changer-btn");
+console.log(button);
+
+// Get Random Color function
+const getRandomColor = () => {
+  // Change the hexadecimal color value
+  return `rgb(${Math.random() * (256 - 0) + 0},${
+    Math.random() * (256 - 0) + 0
+  }, ${Math.random() * (256 - 0) + 0})`;
+};
+// Change the box color when button is clicked
+button.addEventListener("click", () => {
+  box.style.backgroundColor = getRandomColor();
+});
